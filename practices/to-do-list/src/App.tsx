85df2d4bd9e0c54f60516b48ab6a60bf38bbd605
hyeyoungkim <velvetredvelvet91@gmail.com>
2024-03-31 23:28:19 +0900

    초기셋팅 중

---
+++ practices/to-do-list/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ToDoList from './ToDoList';
+import Login from './Login';
+
+function App() {
+	return (
+		<div>
+			<Login />
+			<ToDoList />
+		</div>
+	);
+}
+
+export default App;
---
+++ practices/to-do-list/src/ToDoList.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ToDoList = () => {
+	return <div>TO DO LIST</div>;
+};
+
+export default ToDoList;
